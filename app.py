4a15920f3fb90a4e907748f54b06a03eaa53afd7
Aditya Shewale <141516636+eradityashewale@users.noreply.github.com>
2024-10-02 11:34:19 +0530

    Create log file with stram handler

--- app.py
+++ app.py
@@ -3,14 +3,20 @@ import logging
 
 logger = logging.getLogger()
 
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 stream_handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s %(levelname)s :: %(message)s')
 stream_handler.setFormatter(formatter)
 stream_handler.setLevel(logging.INFO)
 
+file_handler = logging.FileHandler('info.log')
+file_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.INFO)
+
+
 logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
 
 
 logger.debug("This message is important only when debugging the program")
