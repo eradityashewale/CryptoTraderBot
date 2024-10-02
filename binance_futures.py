ba7d9f095e7c9f8ff5a62058d93c228f34a4842c
Aditya Shewale <141516636+eradityashewale@users.noreply.github.com>
2024-10-02 12:20:59 +0530

    Extract contracts from fapi binance.com

--- binance_futures.py
+++ binance_futures.py
@@ -1,4 +1,18 @@
 import logging
-import app
+import requests
+import pprint
  
 logger = logging.getLogger()
+
+def get_contracts():
+    response_object = requests.get('https://fapi.binance.com/fapi/v1/exchangeInfo')
+    print(response_object.status_code)
+    # pprint.pprint(response_object.json())
+    # pprint.pprint(response_object.json()['symbols'])
+
+    contracts = []
+    for contract in response_object.json()['symbols']:
+        contracts.append(contract['pair'])
+    return contracts
+
+print(get_contracts()) 
