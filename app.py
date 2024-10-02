8c922f5ec0353eb1c65b08b29dce140160933582
Aditya Shewale <141516636+eradityashewale@users.noreply.github.com>
2024-10-02 11:29:30 +0530

    Add Debug message

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 import tkinter as tk
+import logging
+
+logger = logging.getLogger()
+
+logger.setLevel(logging.INFO)
+
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)s :: %(message)s')
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.INFO)
+
+logger.addHandler(stream_handler)
+
+
+logger.debug("This message is important only when debugging the program")
+logger.info("This message just show basic information")
+logger.warning("This message is about something you should pay attention to")
+logger.error("This message helps to debug an error that occured in your program")
 
 root = tk.Tk()
 
-root.mainloop()
+root.mainloop() 
